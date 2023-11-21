c5b3a5db427c412c6ae3d57fc1d32420241fd0f7
Boris Marinov <marinovboris@protonmail.com>
2023-11-21 11:18:31 +0200

    stuff

--- build-tags.hs
+++ build-tags.hs
@@ -1,11 +1,13 @@
 #!/usr/bin/env stack
--- stack --resolver lts-19.6 script --package text --package turtle --package containers
+-- stack --resolver lts-19.6 script --package text --package turtle --package containers --package base --package directory
                                     
 {-# LANGUAGE OverloadedStrings #-}  
                                     
 import Turtle                       
 import Data.Text 
 import qualified Data.Set as Set
+import Data.Foldable
+import System.Directory
 
 isMarkdown :: Turtle.FilePath -> Bool
 isMarkdown path = Prelude.not (Prelude.null (match (chars <> "md") (pack (encodeString path))))
@@ -25,18 +27,32 @@ matchLine pattern' line =
     return matching
   where matching = match (pattern') (lineToText line)
 
-
-extractYamlHeader header >>= (matchLine (header ++ ": " *> chars))
-
-tagsInDir :: Turtle.FilePath -> Shell Text
-tagsInDir path = (getFiles path isMarkdown) >>= extractYamlHeader "tags"
-  >>= (\a -> select (a >>= splitOn " "))
+getYamlHeader header shell = (matchLine (header *> chars) shell)
 
 set :: Ord a => Fold a (Set.Set a)
 set = Fold (flip Set.insert) Set.empty id
 
-unique :: Shell a -> Shell (Set.Set a)
-unique shell = (Turtle.fold shell set)
+unique :: Ord a => Shell a -> Shell (Set.Set a)
+unique tags = (Turtle.fold tags set)
 
-main = sh (unique (tagsInDir "_posts")
-  >>= liftIO . print)
+fileContent :: String -> String
+fileContent name = "---\ntag: "++name++"\nlayout: tag\n---\n"
+
+createTagFile :: Text -> IO ()
+createTagFile name = do
+  fileExists <- doesFileExist (location)
+  when (not  fileExists) 
+    ((writeFile (location) (fileContent(unpack name ))) 
+    >> (print ("File created: " ++ unpack name)))
+  where location = "./tags/" ++ (unpack name) ++ ".html"
+
+createTagFiles :: (Set.Set Text) -> Shell ()
+createTagFiles files =  Turtle.liftIO (traverse_ createTagFile files )
+
+main = sh $
+  getFiles "_posts" isMarkdown
+    >>= getYamlHeader "tags: "
+    >>= (\a -> select (a >>= splitOn " "))
+    & mfilter (not . Data.Text.null)
+    & unique
+    >>=createTagFiles
