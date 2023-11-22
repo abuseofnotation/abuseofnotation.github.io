fb2e581a0bcdc9178b1892e90285dfc94e9c6076
Boris Marinov <marinovboris@protonmail.com>
2023-11-22 22:48:56 +0200

    stuff

--- build-jekyll-tags.hs
+++ build-jekyll-tags.hs
@@ -16,25 +16,9 @@ import qualified Data.Set as Set
 import Data.Foldable
 import System.Directory
 
---Checks if a given file is of a given type
-checkFileType :: Pattern Text -> Turtle.FilePath -> Bool
-checkFileType fileType path = Prelude.not (Prelude.null (match (chars <> (fileType)) (pack (encodeString path))))
-
--- Retrieves recursively all files by directory and type (TODO use find)
-getFiles :: Turtle.FilePath -> Pattern Text -> Shell Line
-getFiles path fileType = (mfilter (checkFileType fileType) (lstree path)) >>= Turtle.input 
-
 -- Retrieves the values of all YAML headers in the shell
 getYamlHeader :: Pattern Text -> Line -> [Text]
 getYamlHeader header line = (match (header *> chars) (lineToText line))
-
--- Fold to a set
-set :: Ord a => Fold a (Set.Set a)
-set = Fold (flip Set.insert) Set.empty id
-
--- Returns a set with the unique lines in a shell
-unique :: Ord a => Shell a -> Shell (Set.Set a)
-unique tags = (Turtle.fold tags set)
 
 -- A template 
 fileContent :: String -> String
@@ -49,15 +33,11 @@ createTagFile name = do
     >> (print ("File created: " ++ unpack name)))
   where location = "./tags/" ++ (unpack name) ++ ".html"
 
--- Creates files for all tags
-createTagFiles :: (Set.Set Text) -> Shell ()
-createTagFiles files =  Turtle.liftIO (traverse_ createTagFile files )
-
 main = sh $
-  (getFiles "_posts" "md")
+  (Turtle.find (chars <> ".md") "_posts" )
+    >>= Turtle.input
     >>= return . getYamlHeader "tags: "
     >>= (select . (>>= splitOn " "))
+    & Turtle.nub
     & mfilter (not . Data.Text.null)
-    & unique
-    -- >>= liftIO . print
-    >>=createTagFiles
+    >>= liftIO. createTagFile
